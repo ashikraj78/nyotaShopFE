37e73aeb7002dea26a21d0ffe8aec47281e4aeac
Ashik Raj <ashikraj.78@gmail.com>
2023-08-07 16:12:06 +0530

    adding desing to create page

--- src/components/CreateProductForm.tsx
+++ src/components/CreateProductForm.tsx
@@ -1,9 +1,14 @@
 import { counterStates, setUserData } from "@/redux/counterReducer";
-import React from "react";
+import React, { Dispatch, SetStateAction } from "react";
 import { useForm } from 'react-hook-form';
 import { useDispatch, useSelector } from "react-redux";
 
-function CreateProductForm(){
+   
+    interface Props {
+        setCreateForm: Dispatch<SetStateAction<boolean>>;
+      }
+
+  const CreateProductForm: React.FC<Props> = ({setCreateForm}) => {
     const {userData} = useSelector(counterStates)
     const dispatch = useDispatch()
     const backEndURI = process.env.NEXT_PUBLIC_SERVER_SIDE_URI
@@ -50,9 +55,8 @@ function CreateProductForm(){
             const newUserData = await newUserResponse.json()
             
             dispatch(setUserData({...userData, user: newUserData?.user}))
+            setCreateForm(false)
             
-
-        console.log({newUserData})
         } catch (error) {
             console.error(error);
         }
@@ -61,62 +65,61 @@ function CreateProductForm(){
   
  
     return(
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <form onSubmit={handleSubmit(onSubmit)} className="bg-gray-100 text-center p-4 rounded-lg">
             <div className="my-4">
-                <input  className="buy_inputPlaceholder border-b-2 text-xl pb-2 mb-8  w-1/4"  {...register('title', { required: true })} placeholder="Title" />
-                {errors.title && <span>This field is required</span>}
+                <input  className="buy_inputPlaceholder border-b-2 text-xl p-2 rounded-md  mb-2 shadow   w-1/2"  {...register('title', { required: true })} placeholder="Title" />
+                {errors.title && <span className="text-red-500 block">*This field is required</span>}
             </div>
 
             <div className="my-4">
-                <input  className="buy_inputPlaceholder border-b-2 text-xl pb-2 mb-8  w-1/4" {...register('subTitle', { required: true })} placeholder="Sub Title" />
-                {errors.subTitle && <span>This field is required</span>}
+                <input  className="buy_inputPlaceholder border-b-2 text-xl p-2 rounded-md  mb-2 shadow  w-1/2"  {...register('subTitle', { required: true })} placeholder="Sub Title" />
+                {errors.subTitle && <span className="text-red-500 block">*This field is required</span>}
             </div>
 
             <div className="my-4">
-                <textarea  className="buy_inputPlaceholder border-b-2 text-xl pb-2 mb-8  w-1/4" {...register('description', { required: true })} placeholder="Description" />
-                {errors.description && <span>This field is required</span>}
+                <textarea  rows={4} className="buy_inputPlaceholder border-b-2 text-xl p-2 rounded-md  mb-2 shadow  w-1/2" {...register('description', { required: true })} placeholder="Description" />
+                {errors.description && <span className="text-red-500 block">*This field is required</span>}
             </div>
 
             <div className="my-4">
-                <input  className="buy_inputPlaceholder border-b-2 text-xl pb-2 mb-8  w-1/4" {...register('category', { required: true })} placeholder="Category" />
-                {errors.category && <span>This field is required</span>}
+                <input  className="buy_inputPlaceholder border-b-2 text-xl p-2 rounded-md  mb-2 shadow  w-1/2" {...register('category', { required: true })} placeholder="Category" />
+                {errors.category && <span className="text-red-500 block">*This field is required</span>}
             </div>
 
             <div className="my-4">
-                <input className="buy_inputPlaceholder border-b-2 text-xl pb-2 mb-8  w-1/4" {...register('videoLink', { required: true })} placeholder="Video Link" />
-                {errors.videoLink && <span>This field is required</span>}
+                <input className="buy_inputPlaceholder border-b-2 text-xl p-2 rounded-md  mb-2 shadow w-1/2" {...register('videoLink', { required: true })} placeholder="Video Link" />
+                {errors.videoLink && <span className="text-red-500 block">*This field is required</span>}
             </div>
 
             <div className="my-4">
-                <input className="buy_inputPlaceholder border-b-2 text-xl pb-2 mb-8  w-1/4" {...register('videoTheme', { required: true })} placeholder="Video Theme" />
-                {errors.videoTheme && <span>This field is required</span>}
+                <input className="buy_inputPlaceholder border-b-2 text-xl p-2 rounded-md  mb-2 shadow  w-1/2" {...register('videoTheme', { required: true })} placeholder="Video Theme" />
+                {errors.videoTheme && <span className="text-red-500 block">*This field is required</span>}
             </div>
 
             <div className="my-4">
-                <input  className="buy_inputPlaceholder border-b-2 text-xl pb-2 mb-8  w-1/4" {...register('musicTheme', { required: true })} placeholder="Music Theme" />
-                {errors.musicTheme && <span>This field is required</span>}
+                <input  className="buy_inputPlaceholder border-b-2 text-xl p-2 rounded-md  mb-2 shadow w-1/2" {...register('musicTheme', { required: true })} placeholder="Music Theme" />
+                {errors.musicTheme && <span className="text-red-500 block">*This field is required</span>}
             </div>
 
             <div className="my-4">
-                <input  className="buy_inputPlaceholder border-b-2 text-xl pb-2 mb-8  w-1/4" type="number" {...register('cost', { required: true })} placeholder="Cost" />
-                {errors.cost && <span>This field is required</span>}
+                <input  className="buy_inputPlaceholder border-b-2 text-xl p-2 rounded-md  mb-2 shadow  w-1/2" type="number" {...register('cost', { required: true })} placeholder="Cost" />
+                {errors.cost && <span className="text-red-500 block ">*This field is required</span>}
             </div>
 
             <div className="my-4">
-                <input  className="buy_inputPlaceholder border-b-2 text-xl pb-2 mb-8  w-1/4" type="number" {...register('timeDuration', { required: true })} placeholder="Time Duration" />
-                {errors.timeDuration && <span>This field is required</span>}
+                <input  className="buy_inputPlaceholder border-b-2 text-xl p-2 rounded-md mb-2 shadow  w-1/2" type="number" {...register('timeDuration', { required: true })} placeholder="Time Duration" />
+                {errors.timeDuration && <span className="text-red-500 block ">*This field is required</span>}
             </div>
 
-            <div className="my-4 buy_inputPlaceholder text-xl pb-2 mb-8  w-1/4">
+            <div className="my-4 buy_inputPlaceholder text-xl p-2 rounded-md  mb-2   w-full">
                 <label>
                 Photos Required:
-                <input   type="checkbox" {...register('photosRequired', { required: true })} />
+                <input   type="checkbox" {...register('photosRequired')}  style={{transform: "scale(1.8)"}}  className="ml-2"/>
                 </label>
-                {errors.photosRequired && <span>This field is required</span>}
             </div>
 
-            <div className="my-4">
-                <input type="submit" className="border-2 px-4 py-2 rounded"/>
+            <div className="my-4" >
+                <input type="submit" className="borderTemplate shadow px-4 py-2 text-xl rounded-md cursor-pointer w-1/4"/>
             </div>
         </form>
         
--- src/components/product.tsx
+++ src/components/product.tsx
@@ -16,41 +16,43 @@ function Product(){
                 <p className="text-3xl font-medium">Products</p>
                 <button className="border text-xl text-white font-medium px-8 py-4 rounded primaryColor" onClick={()=> setCreateForm(!createForm)}>Create Product + </button>
             </div>
-           {createForm && (
-            <CreateProductForm />
+           {createForm ? (
+            <CreateProductForm setCreateForm={setCreateForm} />
+           ): (
+            <div className="grid grid-cols-3 gap-4">
+            {productsData.map((product: {_id: string, title: string, cost: number}) => (
+            <div 
+                key={product._id} 
+                className="mb-10 cursor-pointer" 
+                onClick={()=> router.push(`/product/${product._id}`)}
+            >
+                
+                <Image
+                src={"/nyota_explore1_image.svg"}
+                width={400}
+                height={400}
+                alt="Design Explore"
+                />
+                <div className="flex justify-between mt-4">
+                <p className="exploreTextColor text-lg">
+                    {product.title}
+                </p>
+                <p className="exploreTextColor text-xl">{product.cost}/-</p>
+                </div>
+                <p 
+                className=" underline underline-offset-4 decoration-solid primaryTextColor cursor-pointer" 
+                onClick={()=> router.push("/videoDetails")} 
+                >
+                View More
+                </p>
+                <div className="border border-slate-400 border-1 mt-4"></div>
+                <div className="border border-slate-400 border-1 mt-1"></div>
+            </div>
+            ))}
+        </div>
            )}
 
-            <div className="grid grid-cols-3 gap-4">
-                {productsData.map((product: {_id: string, title: string, cost: number}) => (
-                <div 
-                    key={product._id} 
-                    className="mb-10 cursor-pointer" 
-                    onClick={()=> router.push(`/product/${product._id}`)}
-                >
-                    
-                    <Image
-                    src={"/nyota_explore1_image.svg"}
-                    width={400}
-                    height={400}
-                    alt="Design Explore"
-                    />
-                    <div className="flex justify-between mt-4">
-                    <p className="exploreTextColor text-lg">
-                        {product.title}
-                    </p>
-                    <p className="exploreTextColor text-xl">{product.cost}/-</p>
-                    </div>
-                    <p 
-                    className=" underline underline-offset-4 decoration-solid primaryTextColor cursor-pointer" 
-                    onClick={()=> router.push("/videoDetails")} 
-                    >
-                    View More
-                    </p>
-                    <div className="border border-slate-400 border-1 mt-4"></div>
-                    <div className="border border-slate-400 border-1 mt-1"></div>
-                </div>
-                ))}
-            </div>
+           
 
 
           
